1f46d3c2e0f5093570af515825dbbdfa075e5249
Léo Vansimay <leo.vansimay@insa-cvl.fr>
2021-12-12 13:24:05 -0500

    Ajout de la configuration pour OpenCL, ajout de code pour chercher les périphériques compatibles

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 
 
-#include <CL/cl.h>
+#include <CL/cl.hpp>
+#include <vector>
 
 
 
@@ -40,6 +41,31 @@ void init(int n){
 
 
 void main(){
+
+
+    //Test initial pour vérifier que le PC est compatible avec OpenCL
+    std::vector<cl::Platform> all_platforms;
+    cl::Platform::get(&all_platforms);
+    if (all_platforms.size() == 0) {
+        std::cout << " No platforms found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Platform default_platform = all_platforms[0];
+    std::cout << "Using platform: " << default_platform.getInfo<CL_PLATFORM_NAME>() << "\n";
+
+    std::vector<cl::Device> all_devices;
+    default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
+    if (all_devices.size() == 0) {
+        std::cout << " No devices found. Check OpenCL installation!\n";
+        exit(1);
+    }
+    cl::Device default_device = all_devices[0];
+    std::cout << "Using device: " << default_device.getInfo<CL_DEVICE_NAME>() << "\n";
+
+    cl::Context context({ default_device });
+
+    cl::Program::Sources sources;
+
 
     int debug = 10;
     init(debug);
