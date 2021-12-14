735a84cb8dc28c3cac01c7240f7dbab1c584854a
Léo Vansimay <leo.vansimay@insa-cvl.fr>
2021-12-13 22:20:31 -0500

    génération parallèle de la matrice, 1 thread par ligne

--- Source.cpp
+++ Source.cpp
@@ -10,8 +10,6 @@
 
 int * matr_adj;
 int n = 10;
-int i;
-int j;
 int offset;
 
 /*
@@ -74,16 +72,25 @@ void main(){
     cl::Program::Sources sources;
 
     std::string kernel_code =
-        "void kernel generate(global int* M, global const int* i, global const int* j, global const int* off, global const int* n){"
-        "   if (j[get_global_id(0)] == i[get_global_id(0)])"
-        "   {"
-        "       M[off[get_global_id(0)]] = 0;"
+        "void kernel generate(global int* M, global const int* Itt, global const int* n){"
+        "   __private int o;"
+        "   __private int i = get_global_id(0);"
+        "   __private int j;"
+        "   for(j = 0; j < *n; j++)"
+        "       {"
+        "       o = i * (*n) + j;"
+        "       if (j == i)"
+        "       {"
+        "           M[o] = 0;"
+        "           continue;"
+        "       }"
+        "       if (i < *n - 1)"
+        "       {"
+        "           M[o] = 1;"
+        "           continue;"
+        "       }"
+        "       M[o] = *n + 1; "
         "   }"
-        "   if (i[get_global_id(0)] < *n - 1)"
-        "   {"
-        "       M[off[get_global_id(0)]] = 1;"
-        "   }"
-        "   M[off[get_global_id(0)]] = *n + 1; "
         "}";
 
     sources.push_back({ kernel_code.c_str(), kernel_code.length() });
@@ -102,35 +109,27 @@ void main(){
 
     //Création des buffers
     cl::Buffer buff_M(context, CL_MEM_READ_WRITE, n * n * sizeof(int*));
-    cl::Buffer buff_i(context, CL_MEM_READ_WRITE, sizeof(int) * n);
-    cl::Buffer buff_j(context, CL_MEM_READ_WRITE, sizeof(int) * n);
-    cl::Buffer buff_off(context, CL_MEM_READ_WRITE, sizeof(int) * n);
+    cl::Buffer buff_Itt(context, CL_MEM_READ_WRITE, sizeof(int) * n);
     cl::Buffer buff_n(context, CL_MEM_READ_WRITE, sizeof(int));
     //Checker si on pourrait pas utiliser moins de buffer ?
 
     int* Itt = (int*)calloc(n, sizeof(int));
-    int* Off = (int*)calloc(n * n, sizeof(int));
     for (int k = 0; k < n; k++) {
         Itt[k] = k;
-        Off[k] = k * n + k;
     }
 
     cl::CommandQueue queue(context, default_device);
 
     queue.enqueueWriteBuffer(buff_M, CL_TRUE,0, n * n * sizeof(int*), matr_adj);
-    queue.enqueueWriteBuffer(buff_i, CL_TRUE, 0, n * sizeof(int*), Itt);
-    queue.enqueueWriteBuffer(buff_j, CL_TRUE, 0, n * sizeof(int*), Itt);
-    queue.enqueueWriteBuffer(buff_off, CL_TRUE, 0, n * sizeof(int*), Off);
+    queue.enqueueWriteBuffer(buff_Itt, CL_TRUE, 0, n * sizeof(int*), Itt);
     queue.enqueueWriteBuffer(buff_n, CL_TRUE, 0, sizeof(int), &n);
 
     cl::Kernel generate(program, "generate");
     generate.setArg(0, buff_M);
-    generate.setArg(1, buff_i);
-    generate.setArg(2, buff_j);
-    generate.setArg(3, buff_off);
-    generate.setArg(3, buff_n);
+    generate.setArg(1, buff_Itt);
+    generate.setArg(2, buff_n);
 
-    queue.enqueueNDRangeKernel(generate, cl::NullRange, cl::NDRange(n * n), cl::NullRange);
+    queue.enqueueNDRangeKernel(generate, cl::NullRange, cl::NDRange(n), cl::NullRange);
     queue.finish();
 
     queue.enqueueReadBuffer(buff_M, CL_TRUE, 0, n * n * sizeof(int*), matr_adj);
